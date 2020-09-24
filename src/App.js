730ef2099bcc072597383b41773a989228a237f1
Srijan Shrestha <srijaniw@gmail.com>
2020-09-24 22:00:01 +0545

    Setup firebase configuration Create Sigin button to popup gogle authentication popup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD-fzVxyhQJBlyfauIYojEpabPVoI8cIn4",
+  authDomain: "react-chat-6b6db.firebaseapp.com",
+  databaseURL: "https://react-chat-6b6db.firebaseio.com",
+  projectId: "react-chat-6b6db",
+  storageBucket: "react-chat-6b6db.appspot.com",
+  messagingSenderId: "761977692735",
+  appId: "1:761977692735:web:c049882487841880ee4e8a",
+  measurementId: "G-1GVJRBE6NS"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const [user] = useAuthState(auth);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <section>
+        {/* {user ? <ChatRoom /> : <SignIn />} */}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return(
+    <button
+    onClick={signInWithGoogle}
+    >
+      Sigin in with Google
+    </button>
+  )
 }
 
 export default App;
